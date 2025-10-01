d10220d52507e648980347c3886ac61557c840eb
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-02 00:21:46 +0300

    Allow `virtual_call!` to traverse fields

--- src/macros.rs
+++ src/macros.rs
@@ -191,14 +191,14 @@ macro_rules! this_to_self {
 	};
 }
 
-/// Given an invokation of the form `vt_object => name(...)`,
-/// invoke the virtual method `name`
-/// of the [`VtObject`](crate::VtObject) `vt_object`
+/// Given an invokation of the form `vt_object => field1.field2.func(...)`,
+/// invoke the virtual method `func` while traversing `field`s
+/// of the [`VtObject`](crate::VtObject) `vt_object`'s VTable
 /// with the specified arguments, if any.
 #[macro_export]
 macro_rules! virtual_call {
-	($vt_object:expr => $name:ident($($arg:tt)*)) => {{
+	($vt_object:expr => $name:ident$(.$name1:ident)*($($arg:tt)*)) => {{
 		let vt_object = &$vt_object;
-		($crate::VtObject::vtable(vt_object).$name)($crate::VtObject::as_ptr(vt_object), $($arg)*)
+		($crate::VtObject::vtable(vt_object).$name$(.$name1)*)($crate::VtObject::as_ptr(vt_object), $($arg)*)
 	}};
 }
